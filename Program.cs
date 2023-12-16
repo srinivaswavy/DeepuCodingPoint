d471f61631491fbccd4418d05216098f8acf043d
srinivaswavy <srinivaswavy@gmail.com>
2023-12-16 17:40:10 +0530

    My basic program
    My basic program

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+int moneyAtDeepu = 370;
+int moneyAtSrini = 558;
+int totalMoney = moneyAtDeepu + moneyAtSrini;
+
+
+Console.WriteLine("Deepu has " + moneyAtDeepu + " rupees.");
+Console.WriteLine("Srini has " + moneyAtSrini + " rupees.");
+Console.WriteLine("Total money is " + totalMoney + " rupees");
+
+if (moneyAtDeepu > moneyAtSrini)
+{
+    Console.WriteLine("Deepu smiles...");
+    Console.WriteLine("Srini Cries");
+}
+else
+{
+    Console.WriteLine("Srini smiles..");
+    Console.WriteLine("Deepu cries");
+}
